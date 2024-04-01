3fb7675cce59af3c70b3a306c7aa134d3d3cacb7
Rakhimzhan <zhuba.sz@gmail.com>
2024-04-01 22:13:20 +0600

    fixed bugs (added rule of games)

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,4 +4,5 @@ import greeting from '../src/cli.js';
 import game from '../src/index.js';
 
 const name = greeting();
+console.log('What is the result of the expression?');
 game(name, oneGame);
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -4,4 +4,5 @@ import greeting from '../src/cli.js';
 import game from '../src/index.js';
 
 const name = greeting();
+console.log('Find the greatest common divisor of given numbers.');
 game(name, oneGame);
